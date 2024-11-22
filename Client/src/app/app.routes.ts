866cf46bdd9a3adca26152fd5abddc6cef4c7544
Yousef Abdelhady <yoazazy55@gmail.com>
2024-11-22 11:27:13 +0200

    Add Cart Services and components

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
+import { CartComponent } from './features/cart/cart.component';
 
 export const routes: Routes = [
   {path:"",component: HomeComponent},
   {path:"shop",component: ShopComponent},
   {path:"shop/:id",component: ProductDetailsComponent},
+  {path:"cart",component: CartComponent},
   {path:"not-found",component: NotFoundComponent},
   {path:"server-error",component: ServerErrorComponent},
   {path:"**",redirectTo:"not-found",pathMatch: 'full'},
