5d2b95779c2b210e1ddbc1a01b66df338ef772e2
Yousef Abdelhady <yoazazy55@gmail.com>
2024-12-04 07:30:32 +0200

    admin Component

--- Client/src/app/features/orders/order-detailed/order-detailed.component.ts
+++ Client/src/app/features/orders/order-detailed/order-detailed.component.ts
@@ -4,9 +4,11 @@ import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { AddressPipe } from '../../../shared/pipes/address.pipe';
 import { PaymentCardPipe } from '../../../shared/pipes/payment-card.pipe';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Order } from '../../../shared/models/order';
 import { OrderService } from '../../../core/services/order.service';
+import { AccountService } from '../../../core/services/account.service';
+import { AdminService } from '../../../core/services/admin.service';
 
 @Component({
   selector: 'app-order-detailed',
@@ -26,14 +28,28 @@ import { OrderService } from '../../../core/services/order.service';
 export class OrderDetailedComponent implements OnInit {
   private orderService = inject(OrderService);
   private activatedRoute = inject(ActivatedRoute);
+  private accountService = inject(AccountService);
+  private adminService = inject(AdminService);
+  private router = inject(Router);
   order?: Order;
+  buttonText = this.accountService.isAdmin() ? 'Return to admin' : 'Return to orders'
+
   ngOnInit(): void {
     this.loadOrder();
+  }
+
+  onReturnClick() {
+    this.accountService.isAdmin()
+      ? this.router.navigateByUrl('/admin')
+      : this.router.navigateByUrl('/orders')
   }
   loadOrder() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (!id) return;
-    this.orderService.getOrderDetailed(+id).subscribe({
+    const loadOrderData = this.accountService.isAdmin()
+      ? this.adminService.getOrder(+id)
+      : this.orderService.getOrderDetailed(+id);
+    loadOrderData.subscribe({
       next: order => this.order = order
     })
   }
